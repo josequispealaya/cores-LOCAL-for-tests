18ec76f30ae2b66425229bbaa135909436af2adf
fnirino <felipenirino@gmail.com>
2023-05-01 16:13:41 -0300

    agrego runner para teste de and

--- rtl/examples/comb/and/and.vhd
+++ rtl/examples/comb/and/and.vhd
@@ -1,19 +1,19 @@
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
-entity and is
+entity myAnd is
 	port (
 		piA, piB : in STD_LOGIC;
 		poZ : out STD_LOGIC
 	);
 
-end and;
+end myAnd;
 
-architecture and_Arch of and is
+architecture arch_myAnd of myAnd is
 
 begin
 
 	poZ <= piA and piB;
 
-end and_Arch;
+end arch_myAnd;
 
