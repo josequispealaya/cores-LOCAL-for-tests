3df692ebacdca7cd843b562e183bdf12d9db6399
fnirino <felipenirino@gmail.com>
2023-05-11 18:43:31 -0300

    Ejemplo corregido

--- rtl/examples/ledtest/ledtest.vhd
+++ rtl/examples/ledtest/ledtest.vhd
@@ -12,6 +12,7 @@ end entity;
 architecture arch_ledtest of ledtest is
 
 signal counter : unsigned (24-1 downto 0);
+signal counter_2 : unsigned (4-1 downto 0);
 constant max_counter : unsigned := to_unsigned(120000000, 24);
 
 begin
@@ -23,6 +24,7 @@ process (CLK)
     if (rising_edge(CLK)) then
         if (counter = max_counter) then
             counter <= (others => '0');
+            counter_2 <= counter_2 + to_unsigned(1, 4);
         else
             counter <= counter + to_unsigned(1, 24);
         end if;
@@ -30,9 +32,9 @@ process (CLK)
 
 end process;
 
-LED0 <= counter(23);
-LED1 <= counter(22);
-LED2 <= counter(21);
-LED3 <= counter(20);
+LED0 <= counter_2(3);
+LED1 <= counter_2(2);
+LED2 <= counter_2(1);
+LED3 <= counter_2(0);
 
 end architecture;
