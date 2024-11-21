2dd7b7afcc0d917c5a20a35374547b7efb9a8339
José Quispealaya <22422826+josequispealaya@users.noreply.github.com>
2024-11-21 15:42:30 -0300

    Actualizar rutas en run_tests.py

--- run_tests.py
+++ run_tests.py
@@ -187,8 +187,6 @@ def test_cocotb(dut, waves=False):
             runner.build(
                 verilog_sources = [module_path, config_waveform_dump(tmp_dir, module)],
                 hdl_toplevel = module,
-                #MODIFICAMOS
-                #build_dir=module_dir,
                 build_dir = module_dir,
                 build_args = ["-f", os.path.abspath(ICARUS_CFG_FILE)],
             )
