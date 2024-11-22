6ec527863a022dd141ba9aeb470da435d1d03065
José Quispealaya <22422826+josequispealaya@users.noreply.github.com>
2024-11-21 23:34:16 -0300

    Agregar rutas de los .v en el icarus.cf

--- run_tests_pipeline.sh
+++ run_tests_pipeline.sh
@@ -4,7 +4,7 @@
 set -e
 
 # Imprimir mensaje antes de listar los archivos
-echo "Imprimiendo archivos .py en /code/tests/"
+## echo "Imprimiendo archivos .py en /code/tests/"
 TEST_FILES=$(ls /code/tests/*.py)
 
 ##cho "Verificando si el script run_cocotb_tests.sh existe..."
