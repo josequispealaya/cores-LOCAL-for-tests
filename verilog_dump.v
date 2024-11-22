573b9dfe118fca1e94ea6a94671a6a0f1b58e470
José Quispealaya <22422826+josequispealaya@users.noreply.github.com>
2024-11-22 01:07:33 -0300

    Update run_tests.py

--- verilog_dump.v
+++ verilog_dump.v
@@ -1,4 +1,21 @@
 module cocotb_icarus_dump();
+//PRUEBA
+
+reg clk;
+reg reset;
+reg uartrx;  // <--- Asegúrate de que esté declarado aquí
+wire [7:0] data_out;
+
+// Instancia del módulo bajo prueba
+uart_rx uut (
+    .clk(clk),
+    .reset(reset),
+    .uartrx(uartrx),  // <--- Conecta correctamente la señal
+    .data_out(data_out)
+);
+
+// FIN DE PRUEBA
+
 initial begin
     $dumpfile("waveform.vcd");
     $dumpvars(0, REPLACE_WITH_MODULE_NAME);
