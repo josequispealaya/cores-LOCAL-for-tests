e3ce9abf435dabd200d1acfa620231533ae26f98
José Quispealaya <22422826+josequispealaya@users.noreply.github.com>
2024-11-22 03:53:30 -0300

    Reseteara Cero todos los valores

--- run_cocotb_tests.sh
+++ run_cocotb_tests.sh
@@ -8,20 +8,6 @@ if [ ! -z $2 ]; then
     WAVES="--waves"
 fi
 
-# Mensajes de depuración
-
-## echo "EL DUT ANTES DE SER ENVIADO ES: $DUT"
-# Aquí imprime --dut test_uartRx    y    --dut test_uartTx     
-#echo "WAVES: $WAVES"
-#echo "PYTHONPATH: $PYTHONPATH"
-
-
 export PYTHONPATH=tests
-
-#AGREGAMOS ..
-##rm -rf build
-##mkdir -p build
-#
-
 python3 /code/run_tests.py $DUT $WAVES
 
